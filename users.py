eaf1962e8d44f6cb46fb76039ec7cf91748ff08d
Francis PAN <frpan@outlook.fr>
2024-11-13 18:29:38 +0100

    Update users.py
    Mise à jour du code 13/11/2024

--- users.py
+++ users.py
@@ -1,20 +1,12 @@
 from auth import connexion
+from auth import sudo_command_user
 
-def command_user(client_ssh, username, password, sudo_password):
-    command = (f"sudo -S useradd -m {username} -p {password}")
-
-    stdin, stdout, stderr = client_ssh.exec_command(command ,get_pty=True)
-    stdin.write(sudo_password + '\n')
-    stdin.flush()
-    stdout = stdout.readlines()
-    errors = stderr.read().decode()
-    return stdout, errors
 
 # Crée un utilisateur
 def create_user(host, user, mdp, sudo_password, username, password):
 
     client_ssh = connexion(host, user, mdp)
-    stdout, errors = command_user(client_ssh, username, password, sudo_password) 
+    stdout, errors = sudo_command_user(client_ssh, username, password, sudo_password) 
     if errors:
         print(f"Erreur : {errors}")
     else:
